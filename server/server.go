cc659f54b4e990eea3fee38a31c49c31493fb43e
Squid-Logic <Luisriley005@gmail.com>
2025-03-23 05:54:38 +0000

    Basic Cli client and the server
    client can be distrubted to anyone and then you host the server and users should be able to connect using the host url

---
+++ server/server.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"sync"
+)
+
+// Define a structure to hold the client's connection and ID
+type Client struct {
+	conn *websocket.Conn
+	id   string
+}
+
+var clients = make(map[string]*Client) // Store all clients
+var clientsMutex sync.Mutex            // Mutex to handle concurrent access to clients map
+
+// Broadcast a message to all clients except the sender
+func broadcastMessage(message string, senderID string) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+
+	for id, client := range clients {
+		// Don't send the message to the sender
+		if id != senderID {
+			err := client.conn.WriteMessage(websocket.TextMessage, []byte(message))
+			if err != nil {
+				log.Println("Error sending message to client", id, ":", err)
+			}
+		}
+	}
+}
+
+// Define the WebSocket upgrader to handle the HTTP to WebSocket upgrade
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all connections
+	},
+}
+
+func handleClientConnection(w http.ResponseWriter, r *http.Request) {
+	// Upgrade the HTTP connection to a WebSocket connection
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Error upgrading connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Generate a unique ID for the client (this could be more sophisticated)
+	clientID := fmt.Sprintf("%s", conn.RemoteAddr())
+
+	// Add the new client to the list
+	clientsMutex.Lock()
+	clients[clientID] = &Client{conn: conn, id: clientID}
+	clientsMutex.Unlock()
+
+	defer func() {
+		// Remove the client when they disconnect
+		clientsMutex.Lock()
+		delete(clients, clientID)
+		clientsMutex.Unlock()
+	}()
+
+	// Listen for incoming messages from the client
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			break
+		}
+
+		// Convert the message from byte array to string
+		message := fmt.Sprintf("Client %s says: %s", clientID, string(msg))
+
+		// Broadcast the message to all other clients, including the sender's ID
+		broadcastMessage(message, clientID)
+	}
+}
+
+func main() {
+	// Handle incoming HTTP requests and upgrade them to WebSocket connections
+	http.HandleFunc("/", handleClientConnection)
+
+	// Start the server and listen on port 8080
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
